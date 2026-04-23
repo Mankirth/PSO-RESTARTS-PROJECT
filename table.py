2136814f65a00ab83c5037f745f320b62c37971d
Mankirth <159180334+Mankirth@users.noreply.github.com>
2026-04-23 16:56:36 -0400

    cleanup commit

--- table.py
+++ table.py
@@ -12,18 +12,16 @@ columns = ('PSO Mean', 'PSO StdDev', 'EPSO Mean', 'EPSO StdDev', "%-Diff", "Pair
 rows = ['1','2','3','4','5','6','7','8','9','10','11','12']
 errors = [300, 400, 600, 800, 900, 1800, 2000, 2200, 2300, 2400, 2600, 2700]
 for i in range(12):
-    fileResultsDetailsData = pd.read_csv("PSOs 25\\experiment_details.csv")
-    objective_name = "F" + str(i+1)
-    optimizer_name = "PSO"
-    detailedData = fileResultsDetailsData[
-        (fileResultsDetailsData["Optimizer"] == optimizer_name)
-        & (fileResultsDetailsData["objfname"] == objective_name)
-    ]
-    detailedData = detailedData["Iter" + str(2000)]
+    detailedData = []
+    for y in range(30):
+        with open('CEC2022 - FTT-PSO\CEC2022 - FTT-PSO\CEC22-F'+str(i+1)+'\Mcfstpso'+str(y)+'-CEC22-F'+str(i+1)+'-20.txt') as f:
+            mini = f.readline()
+            for x in f:
+                if float(x) < float(mini):
+                    mini = x
+            detailedData.append(float(mini) - errors[i])
     detailedData = np.array(detailedData).T.tolist()
-    data.append(detailedData)
-    for t in range(30):
-        detailedData[t] -= errors[i]
+
     gwoData = detailedData.copy()
 
     fileResultsDetailsData = pd.read_csv("PSOs 25\\experiment_details.csv")
@@ -41,20 +39,37 @@ for i in range(12):
         detailedData[t] -= errors[i]
     gwomData = detailedData.copy()
 
-    total = 0
-    sampleDiffs = 0
-    diffs = np.zeros(30)
+
+    if(np.std(gwoData) > np.std(gwomData)):
+        fValue = np.std(gwoData) / np.std(gwomData)
+    else:
+        fValue = np.std(gwomData) / np.std(gwoData)
+    print('F' + str(i+1) + ' fValue: ' + str(fValue))
+    meanDiffs = np.mean(gwoData) - np.mean(gwomData)
+    gwoDiff = 0
     for t in range(30):
-        sampleDiffs += gwoData[t] - gwomData[t]
-    sampleMean = sampleDiffs / 30
-    #sampleMean = np.mean(gwomData) - np.mean(gwoData)
+        gwoDiff += gwoData[t] - np.mean(gwoData)
+
+    gwomDiff = 0
     for t in range(30):
-        total += np.pow(((gwoData[t]-gwomData[t]) - sampleMean), 2)
-    sampleStd = np.sqrt(total / (29))
-    testStat = sampleMean/(sampleStd/np.sqrt(30))
+        gwomDiff += np.abs(gwoData[t] - np.mean(gwomData))
+    pooledVariance = (gwoDiff + gwomDiff) / 58
+
+    testStat = meanDiffs / np.sqrt(pooledVariance * ((1/30) + (1/30)))
+
+
+    # total = 0
+    # sampleDiffs = 0
+    # diffs = np.zeros(30)
+    # for t in range(30):
+    #     sampleDiffs += gwoData[t] - gwomData[t]
+    # sampleMean = sampleDiffs / 30
+    # #sampleMean = np.mean(gwomData) - np.mean(gwoData)
+    # for t in range(30):
+    #     total += np.pow(((gwoData[t]-gwomData[t]) - sampleMean), 2)
+    # sampleStd = np.sqrt(total / (29))
+    # testStat = sampleMean/(sampleStd/np.sqrt(30))
     percentDiff = ((1 - (np.mean(gwomData)/np.mean(gwoData))) * 100).round(4)
-    if(i+1 == 1):
-        percentDiff = -1*((1 - (np.mean(gwoData)/np.mean(gwomData))) * 100).round(4)
     line.append([np.mean(gwoData).round(4), np.std(gwoData).round(4), np.mean(gwomData).round(4), np.std(gwomData).round(4), percentDiff, testStat.round(4)])
 cellText = line
 
@@ -72,6 +87,6 @@ ax.table(rowLabels=rows,
         loc='center')
 fig.tight_layout()
 
-fig_name = "PSOs 25\\" + "table.png"
+fig_name = "PSOs 25\\" + "FTTtable.png"
 plt.savefig(fig_name, bbox_inches="tight")
 plt.clf()
